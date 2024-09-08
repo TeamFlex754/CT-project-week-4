4a39085b727661582d918c96a4493642e0bae02f
TeamFlex754 <ians.mcvay@gmail.com>
2024-09-08 09:49:20 -0700

    added comments

--- src/GradeStatistics.java
+++ src/GradeStatistics.java
@@ -4,6 +4,7 @@ public class GradeStatistics {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Variables to store grades
         int numTotalGrades = 10;
         float maxGrade = 0;
         float minGrade = 100;
@@ -11,10 +12,10 @@ public class GradeStatistics {
         float userGrade;
         float averageGrade;
 
-
+        // For-loop for user grade input and organizing values as floats
         for (int i = 0; i < numTotalGrades; i++) {
             System.out.println("Enter your grade " + (i + 1) + ": ");
-            userGrade = scanner.nextFloat();
+            userGrade = scanner.nextFloat();  // Sets input as userGrade
 
             if (userGrade > maxGrade) {
                 maxGrade = userGrade;
@@ -22,9 +23,9 @@ public class GradeStatistics {
             if (userGrade < minGrade) {
                 minGrade = userGrade;
             }
-            gradeSum += userGrade;
+            gradeSum += userGrade;  // Sums userGrade values and sets as new variable gradeSum
         }
-        averageGrade = gradeSum / numTotalGrades;
+        averageGrade = gradeSum / numTotalGrades;  // Equation for average
 
         System.out.println("The max grade is: " + maxGrade);
         System.out.println("The min grade is: " + minGrade);
